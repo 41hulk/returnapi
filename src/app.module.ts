10a28d93e1857e796df4bced60a4dc27f7134129
Ntare Guy <gntare2@gmail.com>
2023-04-13 22:26:23 +0200

    Create db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.prod',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
+      type: (process.env.DB_TYPE as any) || 'postgres',
+      host: process.env.PG_HOST || 'localhost',
+      port: parseInt(process.env.PGPORT) || 5430,
+      username: process.env.PGUSER || 'root',
+      password: process.env.PGPASSWORD || 'root@123',
+      database: process.env.PGDATABASE || 'r3_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: false,
+    }),
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('R3Turn')
+    .setDescription('Calculate you carbon footprint and offset ASAP ')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.enableCors();
+  await app.listen(process.env.PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
